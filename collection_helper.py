eb28a134a10eef13aac60117707692aeb1ff335b
Roy Healy <roy.healy87+gh@gmail.com>
2021-05-26 16:07:09 +0100

    Update collection_helper.py

--- collection_helper.py
+++ collection_helper.py
@@ -2,7 +2,7 @@ import scrapinghub
 
 
 class Collection:
-    def __init__(self, proj_id, collection_name, create=True):
+    def __init__(self, proj_id, collection_name, create=False):
         sh_client = scrapinghub.ScrapinghubClient()
         project = sh_client.get_project(proj_id)
         collections = project.collections
